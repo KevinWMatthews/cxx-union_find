608c893c76819c2a8f002909e909da7fb95395c1
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-08-10 09:18:31 -0700

    Throw exception if UnionFind is empty

--- union_find/src/union_find.cpp
+++ union_find/src/union_find.cpp
@@ -1,7 +1,12 @@
 #include "union_find.h"
+#include <stdexcept>
+
+using namespace std;
 
 UnionFind::UnionFind(size_t n_elements)
 {
+    if (n_elements == 0)
+        throw domain_error("Union must have elements");
 }
 
 bool UnionFind::is_connected(size_t p, size_t q)
--- union_find/test/test_union_find.cpp
+++ union_find/test/test_union_find.cpp
@@ -1,14 +1,22 @@
 #include "union_find.h"
+#include <stdexcept>
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
 
+// Boost requires actual/expected in its throw test?
+
 BOOST_AUTO_TEST_CASE(union_find)
 {
-    UnionFind uf(0);
+    UnionFind uf(1);
+}
+
+BOOST_AUTO_TEST_CASE(union_find_can_not_be_empty)
+{
+    BOOST_REQUIRE_THROW(UnionFind(0), std::domain_error);
 }
 
 BOOST_AUTO_TEST_CASE(empty_union_is_not_connected)
 {
-    UnionFind uf(0);
-    BOOST_REQUIRE_EQUAL(false, uf.is_connected(0, 1));
+    UnionFind uf(1);
+    BOOST_REQUIRE_EQUAL(uf.is_connected(0, 1), false);
 }
