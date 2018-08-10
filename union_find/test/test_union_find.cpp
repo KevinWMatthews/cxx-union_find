a38c21a4aa703cad532d33ce804cf60e9b1db36e
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-08-10 09:19:46 -0700

    Next failing test: point is connected to itself

--- union_find/test/test_union_find.cpp
+++ union_find/test/test_union_find.cpp
@@ -15,8 +15,14 @@ BOOST_AUTO_TEST_CASE(union_find_can_not_be_empty)
     BOOST_REQUIRE_THROW(UnionFind(0), std::domain_error);
 }
 
-BOOST_AUTO_TEST_CASE(empty_union_is_not_connected)
+BOOST_AUTO_TEST_CASE(point_is_connected_to_itself)
 {
     UnionFind uf(1);
-    BOOST_REQUIRE_EQUAL(uf.is_connected(0, 1), false);
+    BOOST_REQUIRE_EQUAL(uf.is_connected(0, 0), true);
 }
+
+// BOOST_AUTO_TEST_CASE(empty_union_is_not_connected)
+// {
+    // UnionFind uf(2);
+    // BOOST_REQUIRE_EQUAL(uf.is_connected(0, 1), false);
+// }
