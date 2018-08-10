aca73cd39b3c6fd2ff2c1431dcb3dc4a8a5a3ad4
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-08-10 09:11:37 -0700

    Extract function definitions from header file

--- union_find/include/union_find.h
+++ union_find/include/union_find.h
@@ -1,11 +1,13 @@
 #ifndef UNION_FIND_INCLUDED
 #define UNION_FIND_INCLUDED
 
+#include <cstddef>
+
 class UnionFind
 {
 public:
-    explicit UnionFind(int) {};
-    bool is_connected(int p, int q) { return false;  };
+    explicit UnionFind(size_t n_elements);
+    bool is_connected(size_t p, size_t q);
 };
 
 #endif
--- union_find/src/union_find.cpp
+++ union_find/src/union_find.cpp
@@ -1,1 +1,10 @@
 #include "union_find.h"
+
+UnionFind::UnionFind(size_t n_elements)
+{
+}
+
+bool UnionFind::is_connected(size_t p, size_t q)
+{
+    return false;
+}
