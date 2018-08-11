a7920376a936f24e0692e015e12b2a4d726fcc94
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-08-11 08:39:17 -0700

    Run multiple benchmarks, add notes on test output

--- README.md
+++ README.md
@@ -31,3 +31,28 @@ If I do this then I might be able to get away with a home-less user.
 # Links
 
 [Boost and CMake](https://cmake.org/cmake/help/v3.7/module/FindBoost.html)
+
+
+### Test logging
+
+Boost supports several useful runtime options:
+```
+-p  --show-progress
+-o  --output_format     HRF (default)
+                        XML
+-r  --report_level      confirm (default)
+                        no
+                        short
+                        detailed
+--detect_memory_leaks   Visual Studio debug mode only?
+--list_content
+-l  --log_level         all
+                        test_suite
+                        ...
+--log_sink              [Broken in Boost 1.6.2](https://svn.boost.org/trac10/ticket/12507)
+                        Use --logger instead.
+```
+
+`--logger=HRF,test_suite,benchmark_union_find.txt`
+CTest suppresses logfile generation.
+logger doesn't catch cpu timer's output.
--- union_find/test/test_performance_union_find.cpp
+++ union_find/test/test_performance_union_find.cpp
@@ -10,7 +10,33 @@ BOOST_AUTO_TEST_CASE(local_performance_test)
     // boost::timer::auto_cpu_timer t("%u seconds, user time\n");
     // boost::timer::auto_cpu_timer t("%s seconds, system time\n");
 
-    for (long i = 0; i < 10000000; ++i)
+    for (long i = 0; i < 1000000; ++i)
+        std::sqrt(123.456L); // burn some time
+
+    // auto_cpu_timer automatically prints the time when it is destroyted
+}
+
+BOOST_AUTO_TEST_CASE(local_performance_test2)
+{
+    boost::timer::auto_cpu_timer t;
+    // boost::timer::auto_cpu_timer t("%w seconds, wall time\n");
+    // boost::timer::auto_cpu_timer t("%u seconds, user time\n");
+    // boost::timer::auto_cpu_timer t("%s seconds, system time\n");
+
+    for (long i = 0; i < 1000000; ++i)
+        std::sqrt(123.456L); // burn some time
+
+    // auto_cpu_timer automatically prints the time when it is destroyted
+}
+
+BOOST_AUTO_TEST_CASE(local_performance_test3)
+{
+    boost::timer::auto_cpu_timer t;
+    // boost::timer::auto_cpu_timer t("%w seconds, wall time\n");
+    // boost::timer::auto_cpu_timer t("%u seconds, user time\n");
+    // boost::timer::auto_cpu_timer t("%s seconds, system time\n");
+
+    for (long i = 0; i < 1000000; ++i)
         std::sqrt(123.456L); // burn some time
 
     // auto_cpu_timer automatically prints the time when it is destroyted
