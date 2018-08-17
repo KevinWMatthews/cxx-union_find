2a5961a68d21561d5e33050fa3ba733417d9fe11
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-08-17 06:39:27 -0700

    Check if element is in range

--- union_find/weighted_quick_union/src/weighted_quick_union.cpp
+++ union_find/weighted_quick_union/src/weighted_quick_union.cpp
@@ -7,9 +7,16 @@ WeightedQuickUnion::WeightedQuickUnion(size_t n_elements)
 {
     if (is_valid_size(n_elements) == false)
         throw domain_error("WeightedQuickUnion");
+
+    size = n_elements;
 }
 
 bool WeightedQuickUnion::is_connected(size_t p, size_t q) const
 {
+    if (is_in_range(p) == false)
+        throw out_of_range("WeightedQuickUnion::is_connected");
+    if (is_in_range(q) == false)
+        throw out_of_range("WeightedQuickUnion::is_connected");
+
     return p == q;
 }
