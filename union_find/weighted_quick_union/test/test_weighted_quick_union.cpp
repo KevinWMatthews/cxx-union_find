8c895bf5461bde3bf93f35e96b03753b176d8e1a
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-08-17 07:59:30 -0700

    Point connection is commutative

--- union_find/weighted_quick_union/test/test_weighted_quick_union.cpp
+++ union_find/weighted_quick_union/test/test_weighted_quick_union.cpp
@@ -41,3 +41,11 @@ BOOST_AUTO_TEST_CASE(can_connect_points)
     BOOST_REQUIRE_EQUAL(weighted_quick_union.is_connected(0, 1), true);
     BOOST_REQUIRE_EQUAL(weighted_quick_union.max_depth(), 1);
 }
+
+BOOST_AUTO_TEST_CASE(two_points_can_be_connected_in_either_order)
+{
+    WeightedQuickUnion weighted_quick_union(2);
+    weighted_quick_union.connect(1, 0);
+    BOOST_REQUIRE_EQUAL(weighted_quick_union.is_connected(0, 1), true);
+    BOOST_REQUIRE_EQUAL(weighted_quick_union.is_connected(1, 0), true);
+}
